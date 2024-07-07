15a6eda67204215c7930681d6db42f2fe9b5b06b
Buffy Joe <okaforbestkid@gmail.com>
2024-07-07 13:46:08 +0100

    recent changes to allow internet permission

--- lib/http_request.dart
+++ lib/http_request.dart
@@ -8,7 +8,7 @@ Future<ProductResponse> fetchProducts() async {
   final String organizationId = '72e52bb29284485da5062323af99c1dc';
 
   final Uri url = Uri.parse(
-      'https://api.timbu.cloud/products?organization_id=$organizationId&Appid=$appId&Apikey=$apiKey');
+      'https://api.timbu.cloud/products?organization_id=72e52bb29284485da5062323af99c1dc&Appid=8KQ9BJ5K0T69D27&Apikey=f29bd80c92c2450b9c1a50e8949142d620240706140028992626');
 
   try {
     final response = await http.get(
